3ff977d36fb7ea1da3cdce1e91d74e46f91dd045
Murat Ertik <dmuratertik1@gmail.com>
2024-12-09 06:52:19 -0800

    The requirements has been added as PDF.

--- Java_Codes/Main.java
+++ Java_Codes/Main.java
@@ -3,7 +3,6 @@ package Java_Codes;
 import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) {
-       
+    public static void main(String[] args) {       
     }
 }
